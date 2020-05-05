a0e07784014e2690db457878451a890216d793c1
B <belicia.taylor@gmail.com>
2020-05-05 22:33:12 +0000

    sagemaker: normalized

--- NLP_Learning/text_normalizer.py
+++ NLP_Learning/text_normalizer.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 
 tokenizer = ToktokTokenizer()
 stopword_list = nltk.corpus.stopwords.words('english')
-nlp = spacy.load('en_core_web_sm', parse=True, tag=True, entity=True)
+nlp = spacy.load('en_core_web_md', parse=True, tag=True, entity=True)
 # nlp_vec = spacy.load('en_vectors_web_lg', parse=True, tag=True, entity=True)
 
 def strip_html_tags(text):
