ff47318fa0aed57718cd2251b167ddb6e1ccaa66
beliciataylor <belicia.taylor@gmail.com>
2020-05-05 00:00:44 -0400

    Create cbow.py

---
+++ NLP_Learning/models/cbow.py
@@ -0,0 +1,70 @@
+from nltk.corpus import gutenberg
+from string import punctuation
+from keras.preprocessing import text
+from keras.utils import np_utils
+from keras.preprocessing import sequence
+import keras.backend as K
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, Lambda
+
+bible = gutenberg.sents('bible-kjv.txt')
+remove_terms = punctuation + '0123456789'
+
+norm_bible = [[word.lower() for word in sent if word not in remove_terms] for sent in bible]
+norm_bible = [' '.join(tok_sent) for tok_sent in norm_bible]
+norm_bible = filter(None, normalize_corpus(norm_bible))
+norm_bible = [tok_sent for tok_sent in norm_bible if len(tok_sent.split()) > 2]
+
+tokenizer = text.Tokenizer()
+tokenizer.fit_on_texts(norm_bible)
+word2id = tokenizer.word_index
+
+# build vocabulary of unique words
+word2id['PAD'] = 0
+id2word = {v:k for k, v in word2id.items()}
+wids = [[word2id[w] for w in text.text_to_word_sequence(doc)] for doc in norm_bible]
+
+vocab_size = len(word2id)
+embed_size = 100
+window_size = 2 # context window size
+
+# build a CBOW (context, target) generator
+def generate_context_word_pairs(corpus, window_size, vocab_size):
+    context_length = window_size*2
+    for words in corpus:
+        sentence_length = len(words)
+        for index, word in enumerate(words):
+            context_words = []
+            label_word = []
+            start = index - window_size
+            end = index + window_size + 1
+
+            context_words.append(
+                [words[i] for i in range(start, end)
+                 if 0 <= i < sentence_length and i != index]
+            )
+            label_word.append(word)
+
+            x = sequence.pad_sequences(context_words, maxlen=context_length)
+            y = np_utils.to_categorical(label_word, vocab_size)
+            yield(x,y)
+
+# build CBOW architecture
+cbow = Sequential()
+cbow.add(Embedding(input_dim=vocab_size, output_dim=embed_size, input_length=window_size*2))
+cbow.add(Lambda(lambda x: K.mean(x, axis=1), output_shape=(embed_size, )))
+cbow.add(Dense(vocab_size, activation='softmax'))
+cbow.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+# Train the model
+for epoch in range(1,6):
+    loss = 0
+    i = 0
+    for x, y in generate_context_word_pairs(corpus=wids, window_size=window_size, vocab_size=vocab_size):
+        i += 1
+        loss += cbow.train_on_batch(x, y)
+        if i % 1000000 == 0:
+            print('Processed {} (context, word) pairs'.format(i))
+    
+    print('Epoch:', epoch, '\tLoss:', loss)
+    print()
