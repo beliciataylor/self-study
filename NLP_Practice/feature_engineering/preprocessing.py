0535e583ec5f74bc8682275f8f9b94953dc81d8f
beliciataylor <belicia.taylor@gmail.com>
2020-05-16 18:27:23 -0400

    update

--- NLP_Practice/feature_engineering/preprocessing.py
+++ NLP_Practice/feature_engineering/preprocessing.py
@@ -4,9 +4,21 @@
 
 # simple text processor
 import nltk
+import re
+import numpy as np
+import pandas as pd
+
+## 
+def corpus_df(corpus, labels):
+    corpus = np.array(corpus)
+    corpus_df = pd.DataFrame({'Document': corpus, 'Category': labels})
+    corpus_df = corpus_df[['Document', 'Category']]
+    return corpus_df
+
+## 
 wpt = nltk.WordPunctTokenizer()
 stop_words = nltk.corpus.stopwords.words('english')
-
+ 
 def normalize_document(doc):
     # lowercase and remove special characters\whitespace
     doc = re.sub(r'[^a-zA-Z\s]', '', doc, re.I|re.A)
@@ -19,3 +31,8 @@ def normalize_document(doc):
     # re-create document from filtered tokens
     doc = ' '.join(filtered_tokens)
     return doc
+## 
+def normalize_corpus(norm_doc):
+    norm_corpus = np.vectorize(normalize_document)
+    norm_corpus = norm_corpus(norm_doc)
+    return norm_corpus
