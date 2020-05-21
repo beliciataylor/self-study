02dd91851b3da9ca8e135dd1e2e762bf38fef675
beliciataylor <belicia.taylor@gmail.com>
2020-05-20 22:02:07 -0400

    update

--- NLP_Learning/model_evaluation_utils.py
+++ NLP_Learning/model_evaluation_utils.py
@@ -18,30 +18,16 @@ from sklearn.metrics import roc_curve, auc
 
 def get_metrics(true_labels, predicted_labels):
     
-    print('Accuracy:', np.round(
-                        metrics.accuracy_score(true_labels, 
-                                               predicted_labels),
-                        4))
-    print('Precision:', np.round(
-                        metrics.precision_score(true_labels, 
-                                               predicted_labels,
-                                               average='weighted'),
-                        4))
-    print('Recall:', np.round(
-                        metrics.recall_score(true_labels, 
-                                               predicted_labels,
-                                               average='weighted'),
-                        4))
-    print('F1 Score:', np.round(
-                        metrics.f1_score(true_labels, 
-                                               predicted_labels,
-                                               average='weighted'),
-                        4))
+    print('Accuracy:', np.round(metrics.accuracy_score(true_labels, predicted_labels),4))
+    print('Precision:', np.round(metrics.precision_score(true_labels, predicted_labels,
+                                                         average='weighted'), 4))
+    print('Recall:', np.round(metrics.recall_score(true_labels, predicted_labels,
+                                                         average='weighted'), 4))
+    print('F1 Score:', np.round(metrics.f1_score(true_labels, predicted_labels,
+                                                         average='weighted'), 4))
                         
 
-def train_predict_model(classifier, 
-                        train_features, train_labels, 
-                        test_features, test_labels):
+def train_predict_model(classifier, train_features, train_labels, test_features, test_labels):
     # build model    
     classifier.fit(train_features, train_labels)
     # predict using model
@@ -50,17 +36,16 @@ def train_predict_model(classifier,
 
 
 def display_confusion_matrix(true_labels, predicted_labels, classes=[1,0]):
-    
     total_classes = len(classes)
     level_labels = [total_classes*[0], list(range(total_classes))]
-
+    
     cm = metrics.confusion_matrix(y_true=true_labels, y_pred=predicted_labels, 
                                   labels=classes)
     cm_frame = pd.DataFrame(data=cm, 
                             columns=pd.MultiIndex(levels=[['Predicted:'], classes], 
-                                                  labels=level_labels), 
+                                                  codes=level_labels), 
                             index=pd.MultiIndex(levels=[['Actual:'], classes], 
-                                                labels=level_labels)) 
+                                                codes=level_labels)) 
     print(cm_frame) 
 
 
