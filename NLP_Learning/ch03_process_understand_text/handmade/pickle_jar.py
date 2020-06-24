e211f9395e0cca53fa24a32574b61e456bdc73cd
beliciataylor <belicia.taylor@gmail.com>
2020-06-24 16:13:15 -0400

    update files

--- NLP_Learning/ch03_process_understand_text/handmade/pickle_jar.py
+++ NLP_Learning/ch03_process_understand_text/handmade/pickle_jar.py
@@ -11,25 +11,27 @@ def strip_html_tags(text):
     return stripped_text
 
 
-sample_text = ("US unveils world's most powerful supercomputer, beats China. " 
-               "The US Has unveiled the world's most powerful supercomputer called 'Summit, " 
-               "beating the previous record-holder China's Sunway TaihuLight. With a peak performance " 
-               "of 200,000 trillion calculations per second, it is over twice as fast as Sunway TaihuLight, " 
-               "which is capable of 93,000 trillion calculations per second. Summit has 4,608 servers, " 
-               "which reportedly take up the size of two tennis courts.")
+sample_text = (
+    "US unveils world's most powerful supercomputer, beats China." 
+    "The US Has unveiled the world's most powerful supercomputer called " 
+    "'Summit', beating the previous record-holder China's Sunway TaihuLight. " 
+    "With a peak performance of 200,000 trillion calculations per second, it " 
+    "is over twice as fast as Sunway TaihuLight,which is capable of 93,000 " 
+    "trillion calculations per second. Summit has 4,608 servers, which "
+    "reportedly take up the size of two tennis courts."
+    )
 
 # from characters
 import unicodedata
 
 def remove_accented_chars(text):
-    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    text = unicodedata.normalize('NFKD', text)\
+        .encode('ascii', 'ignore').decode('utf-8', 'ignore')
     return text
 
 from handmade.contractions import CONTRACTION_MAP
 
-def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
-    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())), flags = re.IGNORECASE|re.DOTALL)
-    def expand_match(contraction):
+def expand_match(contraction):
         match = contraction.group(0)
         first_char = match[0]
         expanded_contraction = contraction_mapping.get(match)\
@@ -37,6 +39,11 @@ def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
             else contraction_mapping.get(match.lower())
         expanded_contraction = first_char + expanded_contraction[1:]
         return expanded_contraction
+
+def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
+    contractions_pattern = re.compile(\
+        '({})'.format('|'.join(contraction_mapping.keys())), \
+        flags = re.IGNORECASE|re.DOTALL)    
     expanded_text = contractions_pattern.sub(expand_match, text)
     expanded_text = re.sub("'", "", expanded_text)
     return expanded_text
